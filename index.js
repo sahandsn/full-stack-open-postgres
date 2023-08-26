542b8a58e913b7e5f061c672e08ab238c4cb1697
sanaeisahand@gmail.com <sanaeisahand@gmail.com>
2023-08-26 23:20:39 +0330

    13.1-13.3 completed

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 require('dotenv').config();
-// const { Sequelize, QueryTypes } = require('sequelize');
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize');
 // const express = require('express');
 
 // const app = express();
 
-// const sequelize = new Sequelize(process.env.ELEPHANT_URL);
+const sequelize = new Sequelize(process.env.ELEPHANT_URL);
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.STRING(255),
+    },
+    url: {
+      type: DataTypes.STRING(2048),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+);
 
 const main = async () => {
   try {
     await sequelize.authenticate();
     console.log('connection to db has been established successfully');
+    const blogs = await Blog.findAll();
+    blogs.forEach(b => {
+        console.log(`${b.dataValues.author}: ${b.dataValues.title}, ${b.dataValues.likes} likes`);
+    })
     sequelize.close();
   } catch (err) {
     console.log('unable to connect to db', err);
@@ -18,11 +54,11 @@ const main = async () => {
 
 main();
 
-// app.get('/api/notes', async (req, res) => {
-//   const notes = await sequelize.query('SELECT * FROM blogs', {
+// app.get('/api/blogs', async (req, res) => {
+//   const blogs = await sequelize.query('SELECT * FROM blogs', {
 //     type: QueryTypes.SELECT,
 //   });
-//   res.json(notes);
+//   res.json(blogs);
 // });
 
 // const PORT = process.env.PORT || 3001;
