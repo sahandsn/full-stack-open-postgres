6b8c7b3d8f51e24e5966fc1363eadfa578853aab
sanaeisahand@gmail.com <sanaeisahand@gmail.com>
2023-09-16 18:01:04 +0330

    13.17 completed

---
+++ migrations/20230916_00_initialize.js
@@ -0,0 +1,82 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      author: {
+        type: DataTypes.STRING(255),
+      },
+      url: {
+        type: DataTypes.STRING(2048),
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: {
+            msg: 'Validation isEmail on username failed',
+          },
+        },
+      },
+      passwordHash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+    });
+    await queryInterface.addColumn('blogs', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('blogs');
+    await queryInterface.dropTable('users');
+  },
+};
--- models/index.js
+++ models/index.js
@@ -3,8 +3,6 @@ const User = require('./users');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
-Blog.sync({ alter: true });
-User.sync({ alter: true });
 
 module.exports = {
   Blog,
